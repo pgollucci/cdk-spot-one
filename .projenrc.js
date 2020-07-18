a0cff3e0709508582a565cc0c391a93644e54203
Pahud Hsieh <pahudnet@gmail.com>
2020-07-18 20:02:17 +0800

    Initial commit

---
+++ .projenrc.js
@@ -0,0 +1,55 @@
+const {
+  JsiiProject,
+  Semver
+} = require('projen');
+
+const AWS_CDK_LATEST_RELEASE = '1.45.0';
+const PROJECT_NAME = 'cdk-serverless-api';
+const PROJECT_DESCRIPTION = 'A sample JSII construct lib for AWS CDK';
+
+const project = new JsiiProject({
+  name: PROJECT_NAME,
+  jsiiVersion: Semver.caret('1.5.0'),
+  description: PROJECT_DESCRIPTION,
+  repository: 'https://github.com/pahud/awscdk-jsii-template.git',
+  authorName: 'Pahud Hsieh',
+  authorEmail: 'hunhsieh@amazon.com',
+  stability: 'experimental',
+  devDependencies: {
+    '@aws-cdk/assert': Semver.pinned(AWS_CDK_LATEST_RELEASE),
+    '@types/jest': Semver.caret('25.2.3'),
+    '@types/node': Semver.caret('14.0.11'),
+  },
+  dependencies: {
+    constructs: Semver.pinned('3.0.3'),
+    '@aws-cdk/core': Semver.pinned(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-apigatewayv2': Semver.pinned(AWS_CDK_LATEST_RELEASE),
+    '@aws-cdk/aws-lambda': Semver.pinned(AWS_CDK_LATEST_RELEASE),
+  },
+  python: {
+    distName: 'cdk-serverless-api',
+    module: 'cdk_serverless_api'
+  }
+});
+
+project.addFields({
+  'keywords': [
+    'cdk',
+    'aws',
+  ]
+});
+
+project.gitignore.exclude(
+  'cdk.context.json',
+  'cdk.out',
+);
+
+
+project.npmignore.exclude(
+  'cdk.context.json',
+  'cdk.out',  
+  'coverage',
+);
+
+
+project.synth();
