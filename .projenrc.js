2e925b2f53f720bec81dd0622765a1e6337139b5
Pahud <pahudnet@gmail.com>
2020-10-05 10:15:55 +0800

    chore(dev): upgrade projen and yarn

--- .projenrc.js
+++ .projenrc.js
@@ -1,25 +1,29 @@
 const {
-  ConstructLibraryAws,
+  AwsCdkConstructLibrary,
+  GithubWorkflow,
 } = require('projen');
 
 const AWS_CDK_LATEST_RELEASE = '1.62.0';
+const PROJECT_NAME = 'cdk-spot-one';
+const PROJECT_DESCRIPTION = 'One spot instance with EIP and defined duration. No interruption.';
+const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
 
-const project = new ConstructLibraryAws({
+
+const project = new AwsCdkConstructLibrary({
   "authorName": "Pahud Hsieh",
   "authorEmail": "pahudnet@gmail.com",
-  "name": "cdk-spot-one",
-  "description": "One spot instance with EIP and defined duration. No interruption.",
+  "name": PROJECT_NAME,
+  "description": PROJECT_DESCRIPTION,
   "repository": "https://github.com/pahud/cdk-spot-one.git",
-
+  antitamper: false,
   keywords: ["cdk", "spot", "aws"],
-
   catalog: {
     twitter: 'pahudnet',
     announce: false,
   },
 
   // creates PRs for projen upgrades
-  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+  // projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
 
   cdkVersion: AWS_CDK_LATEST_RELEASE,
 
@@ -36,6 +40,49 @@ const project = new ConstructLibraryAws({
     distName: 'cdk-spot-one',
     module: 'cdk_spot_one'
   }
+});
+
+
+// create a custom projen and yarn upgrade workflow
+const workflow = new GithubWorkflow(project, 'ProjenYarnUpgrade');
+
+workflow.on({
+  schedule: [{
+    cron: '0 6 * * *'
+  }], // 6am every day
+  workflow_dispatch: {}, // allow manual triggering
+});
+
+workflow.addJobs({
+  upgrade: {
+    'runs-on': 'ubuntu-latest',
+    'steps': [
+      ...project.workflowBootstrapSteps,
+
+      // yarn upgrade
+      {
+        run: `yarn upgrade`
+      },
+
+      // upgrade projen
+      {
+        run: `yarn projen:upgrade`
+      },
+
+      // submit a PR
+      {
+        name: 'Create Pull Request',
+        uses: 'peter-evans/create-pull-request@v3',
+        with: {
+          'token': '${{ secrets.' + AUTOMATION_TOKEN + '}}',
+          'commit-message': 'chore: upgrade projen',
+          'branch': 'auto/projen-upgrade',
+          'title': 'chore: upgrade projen and yarn',
+          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
+        }
+      },
+    ],
+  },
 });
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];
